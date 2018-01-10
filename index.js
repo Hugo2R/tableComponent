fd14b2d6143c4adbfc3032392fb4eb6dd6809e12
Hugo Romero <hromero@interware.com.mx>
2018-01-10 11:15:03 -0600

    CSS added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var index = fs.readFileSync('index.html');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(index);
-//  res.end(index);
   res.end();
-}).listen(9615);
+}).listen(8000);
+
+console.log("File can be seen at http://localhost:8000/")
--- test.js
+++ test.js
@@ -1,82 +1,98 @@
-const ajax = (config) => {
-	const xmlhttp = new XMLHttpRequest()
+	// function to retrieve json formatted data from url
 
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-			if (xmlhttp.status == 200) {
-				config.success(JSON.parse(xmlhttp.responseText))
-			}
-			else {
-				alert('There was an error')
-			}
-		}
-	}
+		const ajax = (config) => {
+			const xmlhttp = new XMLHttpRequest()
 
-	xmlhttp.open(config.protocol||"GET", config.url , true)
-	xmlhttp.send();
-}
-
-const output = document.getElementById('output')
-
-const Table = (props) => {
-
-
-	const filteredColumns = (item,index) => {
-
-		let tempRow = []
-
-		for (let i = props.columns.length - 1; i >= 0; i--) {
-			let { key, format } = props.columns[i]
-			if (item[key]){
-				if (typeof format == 'function') {
-					tempRow[i] = <td key={i}>{format(item[key])}</td>
-				} else if (format == 'text') {
-					tempRow[i] = <td key={i}>{item[key]}</td>
-				} else if (format == 'link') {
-					tempRow[i] = <td key={i}><a href={item[key]}>{item[key]}</a></td>
+			xmlhttp.onreadystatechange = function() {
+				if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+					if (xmlhttp.status == 200) {
+						config.success(JSON.parse(xmlhttp.responseText))
+					}
+					else {
+						alert('There was an error')
+					}
 				}
 			}
+
+			xmlhttp.open(config.protocol||"GET", config.url , true)
+			xmlhttp.send();
 		}
-		return (<tr key={index}>{tempRow}</tr>)
-	}
 
-	const cells = props.data.map(filteredColumns)
+		const output = document.getElementById('output')
 
-	return (<table>
-		<thead><tr>{props.columns.map((itm,idx)=><th key={idx}>{itm.description}</th>)}</tr></thead>
-		<tbody>{cells}</tbody>
-	</table>)
-}
+	// React component - Configurable Table
+	// Receives a data object and a configuration object
 
-const columns = [
-	{
-		key: 'name',
-		description: 'Full name',
-		format: 'text'
-	},
-	{
-		key: 'username',
-		description: 'User name',
-		format: 'text'
-	},
-	{
-		key: 'address',
-		description: 'Address',
-		format: (address) => {return (<p><span>{address.street}</span><br /><span>{`${address.suite} ${address.city}`}</span></p>)}
-	},
-	{
-		key: 'website',
-		description: 'URL',
-		format: 'link'
-	}
-]
+		const Table = (props) => {
 
-ajax({
-	url: 'https://jsonplaceholder.typicode.com/users',
-	success: (data) => {
-		const component = <div>
-			<Table data={data} columns={columns} />
-		</div>
-		ReactDOM.render(component, output)
-	}
-})
+
+		// Mapper function with format logic
+
+			const filteredColumns = (item,index) => {
+
+				let tempRow = []
+
+				for (let i = props.columns.length - 1; i >= 0; i--) {
+					let { key, format } = props.columns[i]
+					if (item[key]){
+						if (typeof format == 'function') {
+							tempRow[i] = <td key={i}>{format(item[key])}</td>
+						} else if (format == 'text') {
+							tempRow[i] = <td key={i}>{item[key]}</td>
+						} else if (format == 'link') {
+							tempRow[i] = <td key={i}><a href={`http://${item[key]}`}>{item[key]}</a></td>
+						}
+					}
+				}
+				return (<tr key={index}>{tempRow}</tr>)
+			}
+
+			const cells = props.data.map(filteredColumns),
+			headers = props.columns.map((itm,idx)=><th key={idx}>{itm.description}</th>)
+
+			return (<table>
+				<thead><tr>{headers}</tr></thead>
+				<tbody>{cells}</tbody>
+			</table>)
+		}
+
+		const columns = [
+			{
+				key: 'name',
+				description: 'Full name',
+				format: 'text'
+			},
+			{
+				key: 'username',
+				description: 'User name',
+				format: (text) => {return (
+					<div>
+						<span> <i className="user-icon" />&nbsp;{text}</span>
+					</div>)}
+			},
+			{
+				key: 'address',
+				description: 'Address',
+				format: (address) => {return (
+					<div>
+						<span>{address.street}</span><br />
+						<span>{`${address.suite} ${address.city}`}</span>
+					</div>)}
+			},
+			{
+				key: 'website',
+				description: 'URL',
+				format: 'link'
+			}
+		]
+
+		ajax({
+			url: 'https://jsonplaceholder.typicode.com/users',
+			success: (data) => {
+				const component = <div>
+					<Table data={data} columns={columns} />
+				</div>
+				ReactDOM.render(component, output)
+			}
+		})
+
